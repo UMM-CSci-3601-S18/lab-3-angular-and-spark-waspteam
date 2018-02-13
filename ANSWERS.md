519e258d35bb520c6b543c7dbaba383aee2c268c
hamer073 <hamer073@morris.umn.edu>
2018-02-13 15:08:24 -0600

    Created a bunch of files for the new todo package

---
+++ ANSWERS.md
@@ -0,0 +1,1 @@
+1) The gitignores are in the server folder, the client folder, and the root directory.  
---
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -0,0 +1,52 @@
+package umm3601.todo;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * A fake "database" of todo info
+ *
+ * Since we don't want to complicate this lab with a real database,
+ * we're going to instead just read a bunch of todo data from a
+ * specified JSON file, and then provide various database-like
+ * methods that allow the `todoController` to "query" the "database".
+ */
+public class TodoDatabase {
+
+  private Todo[] allTodos;
+
+  public TodoDatabase(String todoDataFile) throws IOException {
+    Gson gson = new Gson();
+    FileReader reader = new FileReader(todoDataFile);
+    allTodos = gson.fromJson(reader, Todo[].class);
+  }
+
+  /**
+   * Get the single todo specified by the given ID. Return
+   * `null` if there is no todo with that ID.
+   *
+   * @param id the ID of the desired todo
+   * @return the todo with the given ID, or null if there is no todo
+   * with that ID
+   */
+  public Todo getTodo(String id) {
+    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  /**
+   * Get an array of all the todos satisfying the queries in the params.
+   *
+   * @param queryParams map of required key-value pairs for the query
+   * @return an array of all the todos matching the given criteria
+   */
+  public Todo[] listTodos(Map<String, String[]> queryParams) {
+    Todo[] filteredTodos = allTodos;
+
+    return filteredTodos;
+  }
+
+}
